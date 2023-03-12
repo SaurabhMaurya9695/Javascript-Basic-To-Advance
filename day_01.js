c869810fef65e8ddd8f7a0c6bd66c7cb172db56f
SAURABH MAURYA <saurabhmaurya@SAURABHs-MacBook-Air.local>
2023-03-12 14:35:22 +0530

    Till 8th lecture

---
+++ day_01.js
@@ -0,0 +1,94 @@
+// console.log("hello world" ) ;
+
+/*
+    function Solve(a , b , c) {
+        return a + b + c;
+    }
+
+    console.log(Solve(1, 2,3)) ;
+
+*/
+
+/*
+    let a = 78 ;  78 is called literal
+    console.log(a);
+
+    a ="hello" ;
+    console.log(a);
+*/
+
+/*  Rules of Identifier 
+
+    1 - Letter , digits , underscore allows 
+    2 - Must begin with a, $ , _ 
+    3 - it is case sensitive S and s is totally different ;
+    4 - let 8x = 23  this will throws a syntax error ;
+
+*/
+
+/*  Var , Let & const
+    1 - var is globally while let and const are block level scope 
+    2 - var can be declared again but let can't be declared with same name (data type)
+    3 - const can't be changed 
+*/
+
+/*
+    There are seven types of primitive datatypes or objects in JS
+    Primitive datatypes are : NNSSBBU means ->
+    NULL , Number , String , Symbol , Boolean , BigInt , Undefine 
+    we can define Bigint like this ... let x = BigInt("568") ;
+    or objects is key-value pair 
+    s = {
+        key = "value" ,
+        key2 = 2
+    } ;
+
+    to get the type we have to write like this 
+    let s = "we";
+    console.log(typeof(s));
+*/
+
+/* 
+    it is a const object , but we can't change x but we can change x's values ;
+    const x ={
+        name : "saurabh",
+        city : "lucknow" ,
+        pno : 6392272716
+    }
+    x["friend"] = "vikas";
+    x["name"] = "Saurabh Maurya" ;
+    console.log(x);
+*/
+
+/* operators 
+    let a = 10;
+    console.log(a++ , ++a  , a-- ,  --a  );
+*/
+
+/*  comparision operator
+    let a = 5 , b = "5" ;
+    console.log(a == b); // this only check value
+    console.log(a != b);
+    console.log(a === b); // this checks for type with values 5 or "5" is not same one is string and
+    other is bool
+    console.log(a !== b);
+*/
+
+/*
+    let a =prompt("what is your age") ;
+    console.log(a);   // always return in string 
+*/
+
+/*
+    let a = "43" ;
+    let c = a[1];
+    console.log(c);
+    let y = Number.parseInt(a);
+    console.log(y)
+    y ++ ;
+    console.log(typeof y , y); 
+    let x = toString(y) ;
+    console.log(x);
+*/
+
+
